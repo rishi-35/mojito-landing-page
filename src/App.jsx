7653528ef3a416de4c6f1e83929de1f121b2933e
rishi <rishikonga3@gmail.com>
2025-07-24 14:38:17 +0530

    done with art section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import gsap from 'gsap';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art';
 gsap.registerPlugin(ScrollTrigger,SplitText)
 const App = () => {
   return (
@@ -11,7 +13,8 @@ const App = () => {
     <Navbar/>
     <Hero/>
     <Cocktails/>
-    
+    <About/>
+    <Art/>
     </>
    
   );
